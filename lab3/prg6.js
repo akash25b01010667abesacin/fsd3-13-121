4286eda2c0b6a19a34a46192f15bf8aae82c7f17
AKASH CHOURASIA <akash25b01010667abesacin>
2026-09-16 12:18:47 +0530

    Update lab3 program 6

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,24 +1,35 @@
 import http from 'http'
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/' && req.method === 'GET') {
+
+const server =http.createServer((req,res) => {
+      if (req.url === "/" && req.method === "GET") {
         res.statusCode = 200;
-        res.end("send with 200");
-    }
-    else if (req.url === '/' && req.method === 'POST') {
+        res.end("Get Request");
+      } else if (req.url === "/" && req.method === "POST") {
+        // console.log("Request",req);
+        let body=''
+        req.on('data',(chunk)=>{
+          body+=chunk
+        });
+        req.on("end",()=> {
+          const product=JSON.parse(body);
+          console.log("received product:",product);
+           res.statusCode = 200;
+        // res.end("POST Request");
+            res.end(JSON.stringify({msg:'product added',product}));
+        });
+        
+       
+      } 
+      else if (req.url === "/" && req.method === "PUT") {
         res.statusCode = 200;
-        res.end("send with 200");
-    } else if (req.url === '/' && req.method === 'PUT') {
+        res.end("PUT Request");
+      } else if (req.url === "/" && req.method === "DELETE") {
         res.statusCode = 200;
-        res.end("send with 200");
-    } else if (req.url === '/' && req.method === 'DELETE') {
-        res.statusCode = 200;
-        res.end("send with 200");
-    }
-    else {
+        res.end("DELETE Request");
+      } else {
         res.statusCode = 404;
         res.end("request not found");
-    }
+      }
 });
-
-server.listen(5000, () => console.log("prg6 is running"));
+server.listen(5000,()=>console.log("prg6 is running"));
