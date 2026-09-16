0ab7aedbc3b881f58f43926e7df838644b18e6cd
AKASH CHOURASIA <akash25b01010667abesacin>
2026-09-16 12:20:37 +0530

    Update lab3 program 6

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -7,23 +7,41 @@ const server =http.createServer((req,res) => {
         res.end("Get Request");
       } else if (req.url === "/" && req.method === "POST") {
         // console.log("Request",req);
-        let body=''
-        req.on('data',(chunk)=>{
-          body+=chunk
+        let body = "";
+        req.on("data", (chunk) => {
+          body += chunk;
         });
-        req.on("end",()=> {
-          const product=JSON.parse(body);
-          console.log("received product:",product);
-           res.statusCode = 200;
-        // res.end("POST Request");
-            res.end(JSON.stringify({msg:'product added',product}));
+        req.on("end", () => {
+          const product = JSON.parse(body);
+          console.log("received product:", product);
+
+          res.statusCode = 200;
+          // res.end("POST Request");
+          res.end(JSON.stringify({ msg: "product added", product }));
         });
         
-       
       } 
-      else if (req.url === "/" && req.method === "PUT") {
-        res.statusCode = 200;
-        res.end("PUT Request");
+      else if (req.url.startsWith("/products/") === "/" && req.method === "PUT") {
+        const productID=req.url.split('/').pop();
+        console.log('Update Product id:',productID);
+          let body = "";
+          req.on("data", (chunk) => {
+            body += chunk;
+          });
+          req.on("end", () => {
+            const product = JSON.parse(body);
+            // console.log("received product:", product);
+            product.id=productID
+            res.statusCode=200;
+
+            // res.statusCode = 200;
+            // res.end("POST Request");
+            res.end(JSON.stringify({ msg: "product updated", product }));
+          });
+        
+        
+        //  res.statusCode = 200;
+        // res.end("PUT Request");
       } else if (req.url === "/" && req.method === "DELETE") {
         res.statusCode = 200;
         res.end("DELETE Request");
